7e20a8b5b9c7e496c1a43b44124b439ff0900fc1
Michael Kratzer <mkratzer@mytum.de>
2016-05-28 21:31:43 +0200

    Created ContactListActivity

---
+++ WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/ContactListActivity.java
@@ -0,0 +1,61 @@
+package de.tum.whatsappplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    private String[] nameArray;
+    TableLayout table;
+    List<CheckBox> checkBoxList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+        nameArray = getIntent().getStringArrayExtra("nameArray");
+
+        table = (TableLayout) findViewById(R.id.contactList);
+        if (table != null) {
+            table.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+
+            for (String key : Constants.contacts.keySet()) {
+                Contact c = Constants.contacts.get(key);
+                View contact_item = getLayoutInflater().inflate(R.layout.view_contact_item, table, false);
+                ((ImageView) contact_item.findViewById(R.id.chat_icon)).setImageResource(c.imageID);
+                ((TextView) contact_item.findViewById(R.id.chat_name)).setText(c.name);
+                List<String> nameList = new ArrayList<>(Arrays.asList(nameArray));
+                CheckBox checkBox = (CheckBox) contact_item.findViewById(R.id.checkBox);
+                checkBox.setTag(R.string.tag_checkbox_id, c.name);
+                if(nameList.contains(c.name)) {
+                    checkBox.setChecked(true);
+                }
+                checkBoxList.add(checkBox);
+                table.addView(contact_item);
+            }
+        }
+
+
+    }
+
+    public void selectionDone() {
+        List<String> selectedContacts = new ArrayList<>();
+        for(CheckBox checkBox : checkBoxList) {
+            if (checkBox.isSelected()) {
+                selectedContacts.add((String) checkBox.getTag(R.string.tag_checkbox_id));
+            }
+        }
+        setResult(RESULT_OK, getIntent().putExtra("selectedContacts", (String[])selectedContacts.toArray()));
+        finish();
+    }
+}
