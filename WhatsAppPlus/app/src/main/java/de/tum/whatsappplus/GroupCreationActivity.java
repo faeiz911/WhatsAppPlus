b2f20544b3f856a17d8b0796fb4aad71ed9a1f2f
Michael Kratzer <mkratzer@mytum.de>
2016-05-28 18:07:28 +0200

    finished group creation added contact selection activity

--- WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/GroupCreationActivity.java
+++ WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/GroupCreationActivity.java
@@ -1,13 +1,19 @@
 package de.tum.whatsappplus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class GroupCreationActivity extends AppCompatActivity {
 
+    private static final String TAG = ChatListActivity.class.getName();
     private EditText groupTitle;
 
     @Override
@@ -22,7 +28,20 @@ public class GroupCreationActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("New Group");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         //ImageButton iconButton = (ImageButton) findViewById(R.id.imageButton);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_group_creation, menu);
+        return true;
+    }
+
+    public void onNextClick(View view) {
+        Log.i(TAG, "clicked on " + view.getTag(R.string.tag_chat_id));
+        Intent openContactSelection = new Intent(this, ContactSelectionActivity.class);
+        openContactSelection.putExtra("groupTitle", groupTitle.getText());
+        startActivity(openContactSelection);
     }
 }
