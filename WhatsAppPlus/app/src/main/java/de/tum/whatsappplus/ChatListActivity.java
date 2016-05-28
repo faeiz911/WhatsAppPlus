fa0e5b1986c631d1a94e39e0942a4cd931fb5878
Tom Ladek <tomladek@aol.de>
2016-05-28 14:09:34 +0200

    loading contacts in chat list; chat list layout

--- WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/ChatListActivity.java
+++ WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/ChatListActivity.java
@@ -1,9 +1,15 @@
 package de.tum.whatsappplus;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class ChatListActivity extends AppCompatActivity {
 
@@ -12,6 +18,22 @@ public class ChatListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
 
+        TableLayout table = (TableLayout) findViewById(R.id.chat_list_table);
+        if (table != null) {
+            table.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+
+            for (Contact c : Constants.contacts) {
+                if (c.chat == null) continue;
+
+                View chat1 = getLayoutInflater().inflate(R.layout.view_chat_history_item, table, false);
+                ((ImageView) chat1.findViewById(R.id.chat_icon)).setImageResource(c.imageID);
+                ((TextView) chat1.findViewById(R.id.chat_name)).setText(c.name);
+                ((TextView) chat1.findViewById(R.id.chat_history_last)).setText(c.chat.get(c.chat.size()-1).text);
+                ((TextView) chat1.findViewById(R.id.chat_timestamp)).setText(c.time == null ? "" : c.time);
+
+                table.addView(chat1);
+            }
+        }
     }
 
     @Override
--- WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/Constants.java
+++ WhatsAppPlus/app/src/main/java/de/tum/whatsappplus/Constants.java
@@ -15,20 +15,20 @@ public class Constants {
         messages = new ArrayList<>(Arrays.asList(new Message("Michael", "Lust auf Grillen am Wochenende?"),
                 new Message("self", "Klar, wann gehts los?"), new Message("Michael", "Am Samstag um 18 Uhr"),
                 new Message("self", "Soll ich noch irgendwas mitbringen?"), new Message("Michael", "Nein ich hab schon alles eingekauft")));
-        contacts.add(new Contact("Michael", 0, messages, "19:34"));
+        contacts.add(new Contact("Michael", R.drawable.whatsappplus_person_michael, messages, "19:34"));
         messages = new ArrayList<>(Arrays.asList(new Message("self", "Hast du Zeit heute Abend?"), new Message("self", "Wir wollen in eine Bar gehen"),
                 new Message("Maxi", "Tut mir leid aber ich habe schon was vor"), new Message("self", "Ok, schade vielleicht nächstes mal")));
-        contacts.add(new Contact("Maxi", 1, messages, "15:14"));
+        contacts.add(new Contact("Maxi", R.drawable.whatsappplus_person_maxi, messages, "15:14"));
         messages = new ArrayList<>(Arrays.asList(new Message("self", "Hallo"), new Message("Maria", "")));
-        contacts.add(new Contact("Maria", 2, messages, "17:53"));
+        contacts.add(new Contact("Maria", R.drawable.whatsappplus_person_maria, messages, "17:53"));
         messages = new ArrayList<>(Arrays.asList(new Message("", "")));
-        contacts.add(new Contact("Mina", 3, messages, "12:22"));
+        contacts.add(new Contact("Mina", R.drawable.whatsappplus_person_mina, messages, "12:22"));
         messages = new ArrayList<>(Arrays.asList(new Message("", "")));
-        contacts.add(new Contact("Markus", 4, messages, "22:19"));
-        contacts.add(new Contact("Magda", 5, null, null));
-        contacts.add(new Contact("Manuel", 6, null, null));
-        contacts.add(new Contact("Monika", 7, null, null));
-        contacts.add(new Contact("Miriam", 8, null, null));
-        contacts.add(new Contact("Moritz", 9, null, null));
+        contacts.add(new Contact("Markus", R.drawable.whatsappplus_person_markus, messages, "22:19"));
+        contacts.add(new Contact("Magda", R.drawable.whatsappplus_person_magda, null, null));
+        contacts.add(new Contact("Manuel", R.drawable.whatsappplus_person_manuel, null, null));
+        contacts.add(new Contact("Monika", R.drawable.whatsappplus_person_monika, null, null));
+        contacts.add(new Contact("Miriam", R.drawable.whatsappplus_person_miriam, null, null));
+        contacts.add(new Contact("Moritz", R.drawable.whatsappplus_person_moritz, null, null));
     }
 }
